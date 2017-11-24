c9553eebb0e9bfeb7001cb1381e25b1ac1bbff76
kaerunoko <kaerunoko@users.noreply.github.com>
2017-11-25 00:38:36 +0900

    add environments example

---
+++ src/environments.example/environment.mock.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  mock: true,
+  clientId: 'MOCK',
+  scriptId: 'MOCK',
+  scopes: [],
+  dev: true
+};
---
+++ src/environments.example/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  mock: false,
+  clientId: 'YOUR_CLIENT_ID',
+  scriptId: 'YOUR_SCRIPT_ID',
+  scopes: [
+    'https://www.googleapis.com/auth/admin.directory.group',
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/spreadsheets'],
+  dev: false
+};
